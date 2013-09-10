a19cc6511db0d0148563e32ebb382106b4572778
Nate Lowry <nate.lowry@gmail.com>
2013-09-10 17:16:01 -0500

    added comments

--- DynamicSplashScreen/App.xaml.cs
+++ DynamicSplashScreen/App.xaml.cs
@@ -20,23 +20,31 @@ namespace DynamicSplashScreen
         private Thread SplashThread;
         protected override void OnStartup(StartupEventArgs e)
         {
+            // ManualResetEvent acts as a block.  It waits for a signal to be set.
             ResetSplashCreated = new ManualResetEvent(false);
+
+            // Create a new thread for the splash screen to run on
             SplashThread = new Thread(ShowSplash);
             SplashThread.SetApartmentState(ApartmentState.STA);
             SplashThread.IsBackground = true;
             SplashThread.Name = "Splash Screen";
             SplashThread.Start();
 
+            // Wait for the blocker to be signaled before continuing. This is essentially the same as: while(ResetSplashCreated.NotSet) {}
             ResetSplashCreated.WaitOne();
             base.OnStartup(e);
         }
 
         private void ShowSplash()
         {
+            // Create the window
             AnimatedSplashScreenWindow animatedSplashScreenWindow = new AnimatedSplashScreenWindow();
             splashScreen = animatedSplashScreenWindow;
 
+            // Show it
             animatedSplashScreenWindow.Show();
+
+            // Now that the window is created, allow the rest of the startup to run
             ResetSplashCreated.Set();
             System.Windows.Threading.Dispatcher.Run();
         }
